a832fd86be900a0558bfa1c70e0dc40dc2f7bbd8
Grace Lee <grace_lee1@brown.edu>
2023-05-06 17:19:25 -0400

    works ish?

--- FinalProject/video.py
+++ FinalProject/video.py
@@ -8,9 +8,9 @@ import multiprocessing as mp
 
 NUM_SENSORS = 3
 readings_queue = [deque([]) for _ in range(NUM_SENSORS)]
-files = ["media/radial1.mp4"]
+files = ["media/radial1.mp4", "media/circular1"]
 setpoint = 450
-frametime = 25
+frametime = 100
 
 
 def play_clips(files, mult):
@@ -25,8 +25,8 @@ def play_clips(files, mult):
             continue
         
         cv2.imshow('video', frame)
-
-        if cv2.waitKey(frametime // mult) & 0xFF == ord('q'):
+        print(int(frametime / mult.value))
+        if cv2.waitKey(max(1, int(frametime / mult.value))) & 0xFF == ord('q'):
             break
             
     cap.release()
@@ -44,34 +44,29 @@ def read_serial(q, mult):
         for i in range(NUM_SENSORS):
             srq = q[i]
             if len(srq) > 1:
-                change_to_running_avg[i] = statistics.mean(srq) - data[i]
+                change_to_running_avg[i] = statistics.median(srq) - data[i]
             if len(srq) > 25:
                 srq.popleft()
             srq.append(data[i])
 
         total_avg_change = statistics.mean(change_to_running_avg)
-        if -50 < abs(total_avg_change) < 50:
+        if -100 < abs(total_avg_change) < 100:
             continue
-        
+
+        # larger multipler = faster video = increasing diff = total avg change > 0
         if total_avg_change < 0:
-            mult.value -= .01
-            # mult.value *= 100 // avg_change
-            # print(mult.value + int(total_avg_change))
+            mult.value = max(1, mult.value - 1)
         else:
-            mult.value + .01
-            # print(mult.value * int(total_avg_change))
-        print(mult.value)
+            mult.value = min(180, mult.value + 1)
         
 
 if __name__ == "__main__":
-    # read_serial(readings_queue, 1)
-    # play_clips(files, 2)
-    mult = mp.Value('d', 1.0)
+    mult = mp.Value('d', 1)
     p1 = mp.Process(target = read_serial, args=[readings_queue, mult])
-    # p2 = mp.Process(target = play_clips, args=[files, mult])
+    p2 = mp.Process(target = play_clips, args=[files, mult])
     p1.start() 
-    # p2.start()
+    p2.start()
 
     p1.join()
-    # p2.join()
+    p2.join()
     pass
