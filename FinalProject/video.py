77d0bc0277c83bf62aea5cf5f7224b4d0bf3266e
Grace <grace_lee1@brown.edu>
2023-05-09 02:10:34 -0400

    stream from server

--- FinalProject/video.py
+++ FinalProject/video.py
@@ -3,6 +3,10 @@ import serial
 from collections import deque
 import statistics
 import multiprocessing as mp
+import sys
+import json
+import websockets
+import asyncio
 
 
 # constants
@@ -11,18 +15,28 @@ NOISE_THRESHOLD = 100
 QUEUE_SIZE = 25
 WAIT_TIME = 100 
 FRAME_RATE = lambda x: max(1, int(WAIT_TIME / x))
-PORT = "/dev/cu.usbmodem11201"
+# PORT = "COM3"
 
-# globals
-arduino = serial.Serial(port=PORT)
+# # globals
+# arduino = serial.Serial(port=PORT)
+
 readings_queue = [deque([]) for _ in range(NUM_SENSORS)]
-files = ["./media/radial1.mp4", "./media/circular1.mp4"]
+files = ["./media/Dance 2.mov"]
 
+def read_in():
+    line = sys.stdin.readline()
+    #Since our input would only be having one line, parse our JSON data from that
+    print(json.loads, line)
+    return json.loads(line)
 
 # plays video clips according to multiplier calculated by sensor reader
 def play_clips(files, mult):
     video_index = 0
     cap = cv2.VideoCapture(files[video_index])
+
+    WN = "Breathing Underwater"
+    cv2.namedWindow(WN, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(WN, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -31,7 +45,7 @@ def play_clips(files, mult):
             cap = cv2.VideoCapture(files[video_index % len(files)])
             continue
         
-        cv2.imshow('video', frame)
+        cv2.imshow(WN, frame)
         if cv2.waitKey(FRAME_RATE(mult.value)) & 0xFF == ord('q'):
             break
             
@@ -39,9 +53,11 @@ def play_clips(files, mult):
     cv2.destroyAllWindows()
 
 # reads sensor values from arduino and calulates multiplier to pass to streamer
-def read_serial(q, mult):
+async def read_serial(ws, q, mult):
     while True:
-        data = arduino.readline().decode().rstrip().split(",")
+        msg = await ws.recv()
+        data = msg.split(",")
+        # data = arduino.readline().decode().rstrip().split(",")
         if len(data) < NUM_SENSORS:
             continue
         data = [int(r) for r in data]
@@ -63,15 +79,23 @@ def read_serial(q, mult):
             # larger multipler = faster video = inc diff, total avg change > 0
             mult.value = min(100, mult.value + 1)
         
+async def main(rq, mult):
+    url = "ws://localhost:8081"
+    async with websockets.connect(url) as ws:
+        await read_serial(ws, rq, mult)
+        await asyncio.Future()
 
 if __name__ == "__main__":
-
     mult = mp.Value('d', 1)
-    sensor_reader = mp.Process(target = read_serial, args=[readings_queue, mult])
     video_streamer = mp.Process(target = play_clips, args=[files, mult])
-
-    sensor_reader.start() 
     video_streamer.start()
+    asyncio.run(main(readings_queue, mult))
 
-    sensor_reader.join()
-    video_streamer.join()
+    # mult = mp.Value('d', 1)
+    # sensor_reader = mp.Process(target = asyncio.run, args=[wrapper()])
+
+    # sensor_reader.start() 
+    # # video_streamer.start()
+
+    # sensor_reader.join()
+    # # video_streamer.join()
